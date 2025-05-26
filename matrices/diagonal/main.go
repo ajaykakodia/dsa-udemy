0b35564a6b58bbbaa56adf4aa9afac76e52fbf8c
Ajay Yadav <ajay.kakodia@gmail.com>
2025-05-27 00:55:39 +0530

    adding diagonal matrix program

---
+++ matrices/diagonal/main.go
@@ -0,0 +1,62 @@
+package main
+
+import "fmt"
+
+type diagonalMatrix struct {
+	arr []int
+	row int
+	col int
+}
+
+func NewDiagonalMatrix(row, col int) *diagonalMatrix {
+	return &diagonalMatrix{
+		arr: make([]int, row),
+		row: row,
+		col: col,
+	}
+}
+
+func (d *diagonalMatrix) Get(row, col int) int {
+	if row > d.row && col > d.col {
+		return 0
+	}
+	if row == col {
+		return d.arr[row]
+	}
+	return 0
+}
+
+func (d *diagonalMatrix) Set(row, col, val int) {
+	if row > d.row && col > d.col {
+		return
+	}
+	if row == col {
+		d.arr[row] = val
+	}
+}
+
+func (d *diagonalMatrix) Display() {
+	for i := 0; i < d.row; i++ {
+		for j := 0; j < d.col; j++ {
+			if j == i {
+				fmt.Printf("%d ", d.arr[i])
+			} else {
+				fmt.Printf("%d ", 0)
+			}
+		}
+		fmt.Println("")
+	}
+}
+
+func main() {
+	dm := NewDiagonalMatrix(5, 5)
+	dm.Display()
+	println("--------------------------")
+	dm.Set(0, 0, 8)
+	dm.Set(1, 1, 3)
+	dm.Set(2, 2, 5)
+	dm.Set(3, 3, 4)
+	dm.Set(4, 4, 9)
+	dm.Display()
+	fmt.Println("Element :", dm.Get(2, 2))
+}
