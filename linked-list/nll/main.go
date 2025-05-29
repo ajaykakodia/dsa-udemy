a977472a6f926a0d131e1a49d603e33b66f58f27
Ajay Yadav <ajay.kakodia@gmail.com>
2025-05-29 23:22:51 +0530

    adding insert & recursive insert

--- linked-list/nll/main.go
+++ linked-list/nll/main.go
@@ -51,8 +51,56 @@ func RDisplay(first *node) {
 	RDisplay(first.Next)
 }
 
+func RInsert(cn *node, data, index int) *node {
+	if index == 0 {
+		cd := node{
+			Data: data,
+			Next: cn,
+		}
+		return &cd
+	}
+	cn.Next = RInsert(cn.Next, data, index-1)
+	return cn
+}
+
+func (ll *linkedList) Insert(data, index int) {
+	if index > ll.Len {
+		return
+	}
+	ll.Len++
+	cd := node{
+		Data: data,
+	}
+	if index == 0 {
+		cd.Next = ll.First
+		ll.First = &cd
+	} else {
+		cn := ll.First
+		for i := 0; i < index-1; i++ {
+			cn = cn.Next
+		}
+		cd.Next = cn.Next
+		cn.Next = &cd
+	}
+}
+
 func main() {
 	ll := createLinkedList([]int{3, 6, 4, 8, 10, 14, 17})
 	ll.Display()
-	RDisplay(ll.First)
+	// RDisplay(ll.First)
+	ll2 := linkedList{}
+	ll2.Insert(2, 0)
+	ll2.Insert(3, 1)
+	ll2.Insert(4, 2)
+	ll2.Insert(5, 0)
+	ll2.Insert(7, 2)
+	ll2.Display()
+	ll3 := linkedList{}
+	ll3.First = RInsert(ll3.First, 2, 0)
+	ll3.First = RInsert(ll3.First, 3, 1)
+	ll3.First = RInsert(ll3.First, 5, 2)
+	ll3.First = RInsert(ll3.First, 7, 3)
+	ll3.First = RInsert(ll3.First, 1, 2)
+	ll3.First = RInsert(ll3.First, 10, 3)
+	ll3.Display()
 }
